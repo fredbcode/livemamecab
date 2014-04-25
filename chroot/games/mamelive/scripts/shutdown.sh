94cd665dbd051fddaf42babe1f7730a02623f623
fredbcode <stealth@altern.org>
2014-04-25 17:15:56 +0200

    Clean

--- chroot/games/mamelive/scripts/shutdown.sh
+++ chroot/games/mamelive/scripts/shutdown.sh
@@ -3,7 +3,7 @@
 set -evx
 PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/games/:/usr/X11R6/bin/:/usr/bin/X11/
 
-Xdialog --infobox 'Please Wait --- Shutdown' 0 0 2000 &
-sudo halt
+zenity --info --title="Please Wait --- Shutdown" --timeout=2 
+sudo /bin/halt
 
 
